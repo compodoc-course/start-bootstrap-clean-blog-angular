ebb8f4787d840350b10a9c6e7353fd2e9c4dea52
Anartz Mugika <mugan86@gmail.com>
2019-06-01 14:33:52 +0200

    Define script to production

--- src/app/@core/components/components.module.ts
+++ src/app/@core/components/components.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { YoutubePipe } from '../pipes/youtube.pipe';
 import { DomSecurePipe } from '../pipes/dom-secure.pipe';
 import { SidebarModule } from './sidebar/sidebar.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 const COMPONENTS = [
     FooterComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
 ];
 
 const PIPES = [
